b64cf6fa225ea58252fb5bb8a9ef8842c014bc60
AndreaBizz8 <andrea91.bizz8@gmail.com>
2015-10-14 21:50:13 +0200

    Create 3.4.d

---
+++ 3.4.d
@@ -0,0 +1,11 @@
+letterGrade=int(input("What is your grade?"))
+if letterGrade<60:
+    print("F")
+elif letterGrade >= 90 :
+    print("A")
+elif letterGrade >= 80 :
+    print("B")
+elif leterGrade >= 70 :
+    print("C")
+elif letterGrade >= 60 :
+   print("D")
